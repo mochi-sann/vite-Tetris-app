e8604bf805778d9f7d4171ac909cad0c24b65bfc
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-08-31 13:33:22 +0900

    minopatternでいい感じに作った

---
+++ src/TetrisBlocks.ts
@@ -0,0 +1,71 @@
+// [ 回転できる通り . [ 初期のブロックの場所x  , y]]
+export type MinoPatternType = {
+  RountaionNumber: number;
+  blocks: Array<[number, number]>;
+};
+export const MinoPattern: Array<MinoPatternType> = [
+  { RountaionNumber: 0, blocks: [] }, // 何もなし
+  {
+    // I
+    RountaionNumber: 2,
+    blocks: [
+      [-1, 0],
+      [1, 0],
+      [2, 0],
+    ],
+  },
+  {
+    // Z 右
+    RountaionNumber: 2,
+    blocks: [
+      [-1, 0],
+      [0, 1],
+      [1, 1],
+    ],
+  },
+  {
+    // Z 左
+    RountaionNumber: 2,
+    blocks: [
+      [-1, 0],
+      [0, 1],
+      [1, 1],
+    ],
+  },
+  {
+    // Square
+    RountaionNumber: 2,
+    blocks: [
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ],
+  },
+  {
+    //L字右
+    RountaionNumber: 2,
+    blocks: [
+      [-1, 0],
+      [1, 0],
+      [1, 1],
+    ],
+  },
+  {
+    //L 字 左
+    RountaionNumber: 2,
+    blocks: [
+      [0, 1],
+      [1, 0],
+      [1, -1],
+    ],
+  },
+  {
+    // T
+    RountaionNumber: 2,
+    blocks: [
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ],
+  },
+];
